094858012b34a4b3650342e9d906fd8de2995449
CHINMAY RAI <114671850+chinmayraii@users.noreply.github.com>
2022-12-26 16:23:59 +0530

    database (postgresql) connectivity

--- crudd/crudd/settings.py
+++ crudd/crudd/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'crudd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crud_application',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': '',
+        'PASSWORD': ''
     }
 }
 
